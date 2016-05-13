92bfa05c017fbbdbcd630ef5242ce688125cb75f
Jonathan Stoikovitch <jstoikovitch@betable.com>
2016-05-13 12:01:13 -0700

    adding SHA256

--- ramses.rb
+++ ramses.rb
@@ -3,7 +3,7 @@ class Ramses < Formula
   homepage "http://ramses.tech/"
   url "https://github.com/ramses-tech/debian-ramses/archive/master.tar.gz"
   version "0.2"
-  sha1 "6fc888fc02ce5ca77e82c074c4a3eb3ed71ab66a99ed9314c7a5e56a20b60a27"
+  sha256 "6fc888fc02ce5ca77e82c074c4a3eb3ed71ab66a99ed9314c7a5e56a20b60a27"
 
   def install
     bin.install "ramses"
